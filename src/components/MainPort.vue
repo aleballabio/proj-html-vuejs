5b5aaa83336b87e072d77f74319ad922abeb7cbb
aleballabio <aleballabio05@gmail.com>
2022-04-04 20:40:01 +0200

    starting store tab

--- src/components/MainPort.vue
+++ src/components/MainPort.vue
@@ -2,17 +2,20 @@
   <main>
     <featured-courses />
     <smart-tutoring />
+    <store-port />
   </main>
 </template>
 
 <script>
 import FeaturedCourses from "./FeaturedCourses";
 import SmartTutoring from "./SmartTutoring.vue";
+import StorePort from "./StorePort.vue";
 export default {
   name: "MainPort",
   components: {
     FeaturedCourses,
     SmartTutoring,
+    StorePort,
   },
 };
 </script>
---
+++ src/components/StorePort.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="cont-store">
+      <div class="cont-img"></div>
+      <div class="cont-text">
+        <div class="circle-border-lg"></div>
+        <div class="text">
+          <h3 class="uppercase">Get Started <span>Effortlessly</span></h3>
+          <h1>Enjoy our <span class="green unset-fw">Companionship</span></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StorePort",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/temporary/variables";
+
+.cont-store {
+  display: flex;
+  justify-content: center;
+}
+
+.cont-img {
+  flex-basis: 60%;
+  margin-right: 1rem;
+}
+
+.cont-text {
+  flex-basis: 40%;
+
+  .circle-border-lg {
+    margin-bottom: 1rem;
+  }
+
+  .text {
+    padding-left: 3rem;
+
+    h3 {
+      span {
+        color: $darkblue;
+      }
+    }
+  }
+}
+</style>
